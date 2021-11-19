941f68faddc0a2bd93f7dcd56d76645ee6c89936
Sanju Kamat <sanjukamat123@gmail.com>
2021-11-19 11:04:38 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,182 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.758099734311976,
+        "pitch": 0.08578463931267777,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0308487821766708,
+          "pitch": 0.029479234606260007,
+          "rotation": 1.5707963267948966,
+          "target": "1-2"
+        },
+        {
+          "yaw": 2.5365542368935756,
+          "pitch": -0.013401771090730819,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1463943152026452,
+          "pitch": -0.34616986268289907,
+          "title": "RIVASA EMERALD DELUXE",
+          "text": "<div>Amenities&nbsp;</div><div><br></div><div>- AC</div><div>- TV</div><div>- Sanitized Rooms&nbsp;</div><div>- Daily Housekeeping&nbsp;</div><div>- Free Wifi</div><div>- Swimming pool</div><div>- Roof Top&nbsp; Restaurant &amp; Bar</div><div>- Parking Facility&nbsp;</div><div>- 5 Min drive to the Beach</div><div>- Conference Hall&nbsp;</div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.04996568832746817,
+        "pitch": 0.36861914318511424,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3450581552278358,
+          "pitch": 0.05612789279504682,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.824627939028769,
+        "pitch": 0.1884991017204456,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.079734405088132,
+          "pitch": 0.019382659209142616,
+          "rotation": 0,
+          "target": "3-4"
+        },
+        {
+          "yaw": -0.5521063090917817,
+          "pitch": 0.08980095574615987,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.7598948433136314,
+        "pitch": 0.5023880963695024,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1805158045611499,
+          "pitch": 0.03724514589700689,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "RIVASA EMERALD DELUXE",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
